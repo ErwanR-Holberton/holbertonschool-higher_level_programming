751a3db356085943f26b5517ac705927f46865c6
ErwanR-Holberton <6218@holbertonstudents.com>
2023-09-25 13:46:12 +0200

    use abs instead of if statements

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number} is ", end="")
-if number < 0:
-    last = (-number) % 10
-    print("-", end="")
-else:
-    last = number % 10
+last = abs(number) % 10
 if last > 5:
-    print(f"{last} and is greater than 5")
+    print(f"Last digit of {number} is {last} and is greater than 5")
 elif last == 0:
-    print(f"{last} and is zero")
+    print(f"Last digit of {number} is {last} and is zero")
 else:
-    print(f"{last} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
