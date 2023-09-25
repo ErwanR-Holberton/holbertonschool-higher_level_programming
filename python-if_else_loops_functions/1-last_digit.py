276b796e4113929bdca4903bc364aa8ea197b417
ErwanR-Holberton <6218@holbertonstudents.com>
2023-09-25 13:27:17 +0200

    Fixed case negative number ending by 0

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,10 +4,10 @@ number = random.randint(-10000, 10000)
 last = number % 10
 if number < 0:
     last = 10 - last
+last = last % 10
 if last > 5:
-	print(f"Last digit of {number} is {last} and is greater than 5")
+    print(f"Last digit of {number} is {last} and is greater than 5")
 elif last == 0:
-	print(f"Last digit of {number} is {last} and is zero")
+    print(f"Last digit of {number} is {last} and is zero")
 else:
-	print(f"Last digit of {number} is {last} and is less than 6 and not 0")
-
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
