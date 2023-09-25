aab29bf49c9b6da1b9b4185ad34a5918ff9bba95
ErwanR-Holberton <6218@holbertonstudents.com>
2023-09-25 14:30:07 +0200

    Changed formula for last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 print(f"Last digit of {number} is ", end="")
 if number < 0:
-    number = -(abs(number) % 10)
+    number = (abs(number) % 10)
 else:
     number = abs(number) % 10
 if number > 5:
