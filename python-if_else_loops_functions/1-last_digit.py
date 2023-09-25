5ffd122bb8fcc150e02e713127f4e9987a723c6a
ErwanR-Holberton <6218@holbertonstudents.com>
2023-09-25 15:36:02 +0200

    Changed formula

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 print(f"Last digit of {number} is ", end="")
 if number < 0:
-    number = (abs(number) % 10)
+    number = -int(repr(number)[-1])
 else:
-    number = abs(number) % 10
+    number = int(repr(number)[-1])
 if number > 5:
     print(f"{number} and is greater than 5")
 elif number == 0:
