49b2889348031331468e0c8fad3bd9d7d3e8dd4c
ErwanR-Holberton <6218@holbertonstudents.com>
2023-09-27 19:24:44 +0200

    Added if name = main

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
-for module_name in dir(hidden_4):
-    if not module_name.startswith("__"):
-        print(module_name)
+if __name__ == "__main__":
+    for module_name in dir(hidden_4):
+        if not module_name.startswith("__"):
+            print(module_name)
