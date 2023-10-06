28395f4442781a7fc417f1005bebb5f80977990d
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-06 14:55:05 +0200

    push task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    number_printed = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            number_printed += 1
+        except:
+            break
+    print("")
+    return number_printed
+
