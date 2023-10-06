2479b6f538f334d70590764f1aa73948af4f5478
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-06 14:58:00 +0200

    added except index error

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,8 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             number_printed += 1
-        except:
+        except IndexError:
             break
     print("")
     return number_printed
-
