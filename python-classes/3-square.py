d14ba61d3f69e8382d4c5139fd734afd367a59d6
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-09 14:15:10 +0200

    changed line order

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -5,11 +5,11 @@
 class Square:
     """Definition of square"""
     def __init__(self, size=0):
-        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
-        return(self.__size * self.__size)
+        return (self.__size * self.__size)
