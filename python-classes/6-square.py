70d3b0d0d4e73709c389b91f95a341fc5825b788
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-09 15:49:07 +0200

    added function for check int

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,10 +5,7 @@
 class Square:
     """Definition of square"""
     def __init__(self, size=0, position=(0, 0)):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        check_positive_int(size)
         self.__size = size
         if type(position) is not tuple or position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -37,7 +34,9 @@ class Square:
 
     @size.setter
     def position(self, value):
-        if type(value) is not tuple or value[0] < 0 or value[1] < 0:
+        if type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -54,3 +53,9 @@ class Square:
             for j in range(self.__size):
                 print("#", end="")
             print("")
+
+def check_positive_int(value):
+    if not isinstance(value, int):
+        raise TypeError("size must be an integer")
+    if value < 0:
+        raise ValueError("size must be >= 0")
