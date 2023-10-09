93472856375843c1d59eb7e35704ff3d38c7910b
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-09 14:58:11 +0200

    push task 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Contains a class definition of 'Square'"""
+
+
+class Square:
+    """Definition of square"""
+    def __init__(self, size=0, position=(0, 0)):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        if type(position) is not tuple or position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for i in range(self.__position[1]):
+            print("")
+        for i in range(self.__size):
+            for j in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
