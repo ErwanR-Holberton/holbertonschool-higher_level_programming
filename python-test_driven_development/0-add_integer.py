4eff21d9a145032324b208a10d03d59b8c260328
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-10 13:53:10 +0200

    added comments and one infinity test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+this
+is a
+5 line comment
+"""
+
+
 def add_integer(a, b=98):
+    """
+    add two numbers and return an int sum
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
