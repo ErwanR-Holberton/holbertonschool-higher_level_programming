329e8813ae5e05e8688ec6c42ace47f1948552db
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-11 16:24:38 +0200

    push task 4

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Contains a class definition of a rectangle"""
+
+
+class Rectangle:
+    """Definition of a rectangle"""
+    def __init__(self, width=0, height=0):
+        test_positive_int(width, "width")
+        test_positive_int(height, "height")
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        test_positive_int(value, "height")
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        test_positive_int(value, "width")
+        self.__width = value
+
+    def perimeter(self):
+        if self.__height != 0 and self.__width != 0:
+            return self.__height * 2 + self.__width * 2
+        return 0
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        return (("#" * self.__width + "\n") * self.__height)[0:-1]
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+
+
+def test_positive_int(value, message):
+    if not isinstance(value, int):
+        raise TypeError(message + " must be an integer")
+    if value < 0:
+        raise ValueError(message + " must be >= 0")
