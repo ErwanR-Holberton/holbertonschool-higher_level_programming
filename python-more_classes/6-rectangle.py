dca8748a3225d1cb16be51d3634308936be6d9b9
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-11 16:44:43 +0200

    added blank line

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """Definition of a rectangle"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         test_positive_int(width, "width")
         test_positive_int(height, "height")
