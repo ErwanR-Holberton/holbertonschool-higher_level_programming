83b19775c9d821f4f907d3993eb794d32eb2dbce
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-12 17:09:06 +0200

    separated checks for value 1 and value 0

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -56,5 +56,7 @@ def check_tuple(value):
         raise TypeError("position must be a tuple of 2 positive integers")
     if not isinstance(value[1], int):
         raise TypeError("position must be a tuple of 2 positive integers")
-    if value[0] < 0 or value[1] < 0:
+    if value[0] < 0:
+        raise TypeError("position must be a tuple of 2 positive integers")
+    if value[1] < 0:
         raise TypeError("position must be a tuple of 2 positive integers")
