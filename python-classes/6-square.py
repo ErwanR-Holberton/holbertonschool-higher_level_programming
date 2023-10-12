950e2fbc7bd20b19cc40dd24afb968c4f69d73cc
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-12 13:30:23 +0200

    Changed test order

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -50,9 +50,9 @@ def check_size(value):
 def check_tuple(value):
     if type(value) is not tuple:
         raise TypeError("position must be a tuple of 2 positive integers")
-    if value[0] < 0 or value[1] < 0:
-        raise TypeError("position must be a tuple of 2 positive integers")
     if not isinstance(value[0], int):
         raise TypeError("position must be a tuple of 2 positive integers")
     if not isinstance(value[1], int):
+        raise TypeError("position must be a tuple of 2 positive integers")
+    if value[0] < 0 or value[1] < 0:
         raise TypeError("position must be a tuple of 2 positive integers")
