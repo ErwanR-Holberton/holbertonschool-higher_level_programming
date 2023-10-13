b412b6e8fb1879c1ade059336dfd54319f89d862
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-13 16:07:58 +0200

    Added test with only one value

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -22,5 +22,8 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_empty(self):
         self.assertEqual(max_integer([]), None)
+
+    def test_only_one(self):
+        self.assertEqual(max_integer([77]), 77)
 if __name__ == "__main__":
     unittest.main()
