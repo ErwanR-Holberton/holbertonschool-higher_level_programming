8264ed7f1a8fa946f5b56bcacf1c605c427b47b4
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-16 21:31:08 +0200

    push task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+"""a class"""
+
+
+class BaseGeometry:
+    """a class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """a rectangle class that inherits from basegeometry"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+
+class Square(Rectangle):
+    """square from rectangle"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__width = size
+        self.__height = size
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[{}] {}/{}".format(self.__class__.__name__, self.__width, self.__height)
+
