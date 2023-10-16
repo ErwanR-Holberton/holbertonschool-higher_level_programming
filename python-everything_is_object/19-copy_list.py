3106b80192b74ea69df093397137462cc7830552
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-16 12:42:10 +0200

    Correct mistakes

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def copy_list(a_list):
-    new_list = [0] * len(a_list)
-    for i in range(len(a_list)):
-        new_list[i] = a_list[i]
-    return new_list
+    return a_list[:]
 
