f1e5aae982ed038609f51ba454637303b4efe232
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-18 12:20:59 +0200

    push tash 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that load list from file then add args in list and save it"""
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+new_list = load_from_json_file("add_item.json")
+for i in range(1, len(argv)):
+    new_list.append(argv[i])
+
+save_to_json_file(new_list, "add_item.json")
