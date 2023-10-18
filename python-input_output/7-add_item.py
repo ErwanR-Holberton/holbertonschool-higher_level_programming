261667ede56f2dfd84a56f19585994465b42bedb
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-18 12:48:29 +0200

    add if name is main

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,8 +4,8 @@ from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-new_list = load_from_json_file("add_item.json")
-for i in range(1, len(argv)):
-    new_list.append(argv[i])
-
-save_to_json_file(new_list, "add_item.json")
+if __name__ == "__main__":
+    new_list = load_from_json_file("add_item.json")
+    for i in range(1, len(argv)):
+        new_list.append(argv[i])
+    save_to_json_file(new_list, "add_item.json")
