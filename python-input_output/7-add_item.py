b5b05500aa9641fb3b035bde19b09c6cd52d3b6d
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-18 13:11:08 +0200

    added try to avoid probleme when file dont exist

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,7 +5,10 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
-    new_list = load_from_json_file("add_item.json")
+    try:
+        new_list = load_from_json_file("add_item.json")
+    except:
+        new_list = []
     for i in range(1, len(argv)):
         new_list.append(argv[i])
     save_to_json_file(new_list, "add_item.json")
