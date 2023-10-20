2705d34e271f9f01024b5e0f8a1fdefab34af0d8
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-20 21:08:52 +0200

    Documentation

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""in this file i use a class to keep track of the amount of calls to funcio"""
+
+
 class MagicString:
+    """class with a public value"""
     i = 0
     @classmethod
     def __init__(cls):
         cls.i += 1
 
 
-
 def magic_string():
+    """create an object to count calls, print n*bestschool"""
     item = MagicString()
     return ("BestSchool, " * MagicString.i)[:-2]
