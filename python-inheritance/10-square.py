054868c778ff8210cb54bb94993f55c6654b7b4d
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-20 09:52:07 +0200

    Try with super init

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,36 +1,12 @@
 #!/usr/bin/python3
 """a class"""
-
-
-class BaseGeometry:
-    """a class"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """a rectangle class that inherits from basegeometry"""
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """square from rectangle"""
     def __init__(self, size):
         self.integer_validator("size", size)
-        self.__size = size
+        super().__init__(size, size)
 
-    def area(self):
-        return self.__size * self.__size
+
