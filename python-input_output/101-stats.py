746fda1ea8bdcfd68bb0de750582e0f4b14dd090
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-20 19:34:31 +0200

    push task 101

---
+++ python-input_output/101-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+from sys import stdin
+i = 0
+error = {}
+error['200'] = 0
+error['301'] = 0
+error['400'] = 0
+error['401'] = 0
+error['404'] = 0
+error['405'] = 0
+error['500'] = 0
+total_size = 0
+for line in stdin:
+    for char in range(0, len(line)):
+        if line[char] == ' ':
+            ip = line[0:char]
+            line = line[char + 3:]
+            break
+    for char in range(0, len(line)):
+        if line[char] == ']':
+            date = line[0:char + 1]
+            line = line[char + 2:]
+            break
+    for char in range(1, len(line)):
+        if line[char] == '\"':
+            file = line[0:char + 1]
+            line = line[char + 2:]
+            break
+    for char in range(0, len(line)):
+        if line[char] == ' ':
+            code = line[0:char]
+            line = line[char + 1:]
+            break
+    size = int(line)
+    for key, value in error.items():
+        if key == code:
+            error[key] += 1
+    total_size += size
+    i += 1
+    if i % 10 == 0:
+        print("File size: {}".format(total_size))
+        for key, value in error.items():
+            if value != 0:
+                print("{}: {}".format(key, value))
