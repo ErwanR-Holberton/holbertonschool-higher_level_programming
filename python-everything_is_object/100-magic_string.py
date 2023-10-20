ba2f2dd7260d55b3fbf170ed9094acfa11573d07
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-20 21:16:40 +0200

    better like this

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-"""in this file i use a class to keep track of the amount of calls to funcio"""
-
-
-class MagicString:
-    """class with a public value"""
-    i = 0
-    @classmethod
-    def __init__(cls):
-        cls.i += 1
-
-
 def magic_string():
-    """create an object to count calls, print n*bestschool"""
-    item = MagicString()
-    return ("BestSchool, " * MagicString.i)[:-2]
+    magic_string.n = getattr(magic_string, "n", 0) + 1
+    return ("BestSchool, " * magic_string.n)[:-2]
