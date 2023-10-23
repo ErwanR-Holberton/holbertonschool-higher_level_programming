ef074122654edb23aa9fd0985ebc4de97beaccc4
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-23 21:49:59 +0200

    added class square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,4 +6,9 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Square class"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x=0, y=0, id=None)
+        super().__init__(size, size, x, y, id=None)
+
+    def __str__(self):
+        """returns description of square"""
+        return "[{}] ({}) {}/{} - {}".format(
+            self.__class__.__name__, self.id, self.x, self.y, self.width)
