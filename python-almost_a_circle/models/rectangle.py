7d3ee368d702f7fb8a30b604265841606185d15d
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-23 12:28:01 +0200

    corrected name height

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,7 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
-        self.__heught = height
+        self.__height = height
         self.__x = x
         self.y = x
 
