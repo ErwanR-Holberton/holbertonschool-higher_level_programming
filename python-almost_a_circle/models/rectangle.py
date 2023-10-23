507c1f795e802802c2b17defe4f1474b58564c43
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-23 12:26:19 +0200

    Added rectangle inheriting from Base

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""define a class inheriting from Base"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """define a class inheriting from Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__heught = height
+        self.__x = x
+        self.y = x
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
