5b50a40ea39e7c276add94caff088030d0314d7a
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-23 20:39:30 +0200

    Added ifs in function update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -77,12 +77,17 @@ class Rectangle(Base):
 
     def update(self, *args):
         """update attributes"""
-        self.id = args[0]
-        self.width(args[1])
-        self.height(args[2])
-        self.x(args[3])
-        self.y(args[4])
-
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
 
 def is_integer(value, name):
     """check if integer"""
