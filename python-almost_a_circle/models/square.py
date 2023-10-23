dec043ed54d8a625e174d9712fe6af6e07a05714
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-23 22:48:54 +0200

    Added update function

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -21,3 +21,26 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """updates values of square"""
+        if args is not None and len(args) != 0:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.width = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+
