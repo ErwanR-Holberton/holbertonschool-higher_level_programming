66b50be0a6d41c2337eda0fdb1c1dc4bca81f01f
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-24 10:06:41 +0200

    changed json to string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,6 +16,4 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """return json string from list"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
         return json.dumps(list_dictionaries)
