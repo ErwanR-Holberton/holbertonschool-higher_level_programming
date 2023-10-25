508cdbee5739b0c1ec278162b380336424135307
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-25 15:40:56 +0200

    new version of update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -78,17 +78,8 @@ class Rectangle(Base):
 
     def update(self, *args, **kwargs):
         """update attributes"""
-        if args is not None and len(args) != 0:
-            if len(args) > 0:
-                self.id = args[0]
-            if len(args) > 1:
-                self.__width = args[1]
-            if len(args) > 2:
-                self.__height = args[2]
-            if len(args) > 3:
-                self.__x = args[3]
-            if len(args) > 4:
-                self.__y = args[4]
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def to_dictionary(self):
         """returns a dictionary"""
