c21bc0b81955039cdd23456bbd06d10e56052de8
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-25 16:55:38 +0200

    changed condition in tojsonstring

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,7 +17,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """return json string from list"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
