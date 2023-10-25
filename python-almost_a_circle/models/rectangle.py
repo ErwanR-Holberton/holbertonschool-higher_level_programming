0338f1f0f16e164036e1c13ec3e442b2c1996ead
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-25 14:38:21 +0200

    Quest to find pycodestyle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -82,8 +82,13 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """returns a dictionary"""
-        return {'id': self.id, 'width': self.width, 'height': self.height,
-                'x': self.x, 'y': self.y}
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
 
 
 def is_integer(value, name):
