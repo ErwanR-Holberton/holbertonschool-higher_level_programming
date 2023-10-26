04bf24f0af884510cccb0fc262e71e13c9400c70
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-26 22:20:25 +0200

    Added tests for update

--- python-almost_a_circle/tests/test_3_str_display.py
+++ python-almost_a_circle/tests/test_3_str_display.py
@@ -26,6 +26,34 @@ class TestStrDisplay(unittest.TestCase):
         self.assertEqual(r1.display(), print("\n\n ##\n ##\n"))
         self.assertEqual(s1.display(), print("\n\n ##\n ##\n"))
 
+    def test_update(self):
+        r2 = Rectangle(2, 2, 0, 0, 22)
+        s2 = Square(2, 0, 0, 23)
+        r2.update(24, 2, 3, 4, 5)
+        s2.update(25, 8, 9, 10)
+        self.assertEqual(r2.id, 24)
+        self.assertEqual(r2.width, 2)
+        self.assertEqual(r2.height, 3)
+        self.assertEqual(r2.x, 4)
+        self.assertEqual(r2.y, 5)
+        self.assertEqual(s2.id, 25)
+        self.assertEqual(s2.size, 8)
+        self.assertEqual(s2.x, 9)
+        self.assertEqual(s2.y, 10)
+
+        r2.update(x=1, id=52, height=3, width=3, y=2)
+        s2.update(x=1, id=53, size=2, y=2)
+        self.assertEqual(r2.id, 52)
+        self.assertEqual(r2.width, 3)
+        self.assertEqual(r2.height, 3)
+        self.assertEqual(r2.x, 1)
+        self.assertEqual(r2.y, 2)
+        self.assertEqual(s2.id, 53)
+        self.assertEqual(s2.size, 2)
+        self.assertEqual(s2.x, 1)
+        self.assertEqual(s2.y, 2)
+
+
 
 if __name__ == '__main__':
     unittest.main()
