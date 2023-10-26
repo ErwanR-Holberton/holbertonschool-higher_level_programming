01f65fd37f12b41ec8ec814421d75b9a43a1435e
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-26 10:38:19 +0200

    new update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -89,6 +89,9 @@ class Rectangle(Base):
                 self.x = args[3]
             if len(args) > 4:
                 self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def to_dictionary(self):
         """returns a dictionary"""
