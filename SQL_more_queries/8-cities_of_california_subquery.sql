27add1e2ebd1fbc2e804a9c74e09ce2cb6c5b0f4
ErwanR-Holberton <6218@holbertonstudents.com>
2023-11-21 15:35:02 +0100

    Removed column state id

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- create database and table wit reference to other table
 SET @california_id = (SELECT id FROM states WHERE name = 'California');
-SELECT * FROM cities WHERE state_id = @california_id ORDER BY id DESC;
+SELECT id, name FROM cities WHERE state_id = @california_id ORDER BY id ASC;
