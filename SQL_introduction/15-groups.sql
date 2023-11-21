1fb9dc6af2306a135d38461f5ad6168eb19e4539
ErwanR-Holberton <6218@holbertonstudents.com>
2023-11-21 13:52:01 +0100

    Using count instead of rank

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Add label with counter
-SELECT score, RANK() OVER (ORDER BY score ASC) AS number FROM second_table ORDER BY score DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY score DESC;
