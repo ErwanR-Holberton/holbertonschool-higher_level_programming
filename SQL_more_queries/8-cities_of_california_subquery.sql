578b80cf3c0dec7e6c4227d8efb80617de8fce0c
ErwanR-Holberton <6218@holbertonstudents.com>
2023-11-21 15:27:58 +0100

    List californian cities

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,3 @@
+-- create database and table wit reference to other table
+SET @california_id = (SELECT id FROM states WHERE name = 'California');
+SELECT * FROM cities WHERE state_id = @california_id;
