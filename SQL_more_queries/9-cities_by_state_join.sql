65facdad8aa47f5896e25ffb13446e5d1d0fb936
ErwanR-Holberton <6218@holbertonstudents.com>
2023-11-21 16:41:23 +0100

    Sort by cities.id

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list all cities with the states name
-SELECT cities.id, cities.name AS city_name, states.name AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities
 LEFT JOIN states ON cities.state_id=states.id
-ORDER BY id ASC;
+ORDER BY cities.id ASC;
