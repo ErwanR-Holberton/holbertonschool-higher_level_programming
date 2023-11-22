bd142dd4be4892971c3629d32c16281df5870802
ErwanR-Holberton <6218@holbertonstudents.com>
2023-11-22 11:26:00 +0100

    Removed one group

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,5 +4,5 @@ FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = show_id
 LEFT JOIN tv_genres ON genre_id = tv_genres.id
 WHERE name IS NOT NULL
-GROUP BY genre_id, name
+GROUP BY name
 ORDER BY number_of_shows DESC;
