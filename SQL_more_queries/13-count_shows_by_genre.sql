4f3d3f40e814eed81be5abf6b576bbffc6f56906
ErwanR-Holberton <6218@holbertonstudents.com>
2023-11-22 11:27:18 +0100

    changed name as genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- count shows by genre
-SELECT name, COUNT(tv_shows.id) AS number_of_shows
+SELECT name AS genre, COUNT(tv_shows.id) AS number_of_shows
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = show_id
 LEFT JOIN tv_genres ON genre_id = tv_genres.id
-WHERE name IS NOT NULL
-GROUP BY name
+WHERE genre IS NOT NULL
+GROUP BY genre
 ORDER BY number_of_shows DESC;
