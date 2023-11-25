033428c9d1002e20d7836a9b83137a746447a2c4
ErwanR-Holberton <6218@holbertonstudents.com>
2023-11-25 16:43:58 +0100

    Don't execute the code if imported

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,21 +4,22 @@ from sqlalchemy import create_engine, Column, Integer, String, Sequence
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sys import argv
 
+if __name__ == "__main__":
 
-Base = declarative_base()
+    Base = declarative_base()
 
-class State(Base):
-    __tablename__ = 'states'
-    id = Column(Integer, Sequence('state_id_seq'), primary_key=True)
-    name = Column(String(50))
+    class State(Base):
+        __tablename__ = 'states'
+        id = Column(Integer, Sequence('state_id_seq'), primary_key=True)
+        name = Column(String(50))
 
 
-engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]))
-Base.metadata.create_all(engine)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-for state in session.query(State).order_by(State.id).all(): # HERE: no SQL query, only objects!
-    print("{}: {}".format(state.id, state.name))
-session.close()
+    for state in session.query(State).order_by(State.id).all(): # HERE: no SQL query, only objects!
+        print("{}: {}".format(state.id, state.name))
+    session.close()
