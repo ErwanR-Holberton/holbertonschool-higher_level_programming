03cc2adceb627b04e090ea323ab7c9641a9916d5
ErwanR-Holberton <6218@holbertonstudents.com>
2023-11-25 19:56:56 +0100

    Model state from database

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class representing states from a database"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'States'
+    id = Column("id", Integer, primary_key=True)
+    name = Column("name", String(128), nullable=False)
