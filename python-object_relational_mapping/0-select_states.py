43c1e53b3f9d29dd81a4a34a49145aa97fb97817
ErwanR-Holberton <6218@holbertonstudents.com>
2023-11-25 16:49:32 +0100

    Switch to no ORM

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
 import MySQLdb
-from sqlalchemy import create_engine, Column, Integer, String, Sequence
-from sqlalchemy.orm import sessionmaker, declarative_base
 from sys import argv
 
 if __name__ == "__main__":
 
-    Base = declarative_base()
-
-    class State(Base):
-        __tablename__ = 'states'
-        id = Column(Integer, Sequence('state_id_seq'), primary_key=True)
-        name = Column(String(50))
-
-
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    for state in session.query(State).order_by(State.id).all(): # HERE: no SQL query, only objects!
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
