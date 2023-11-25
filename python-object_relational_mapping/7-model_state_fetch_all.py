3254333094c8f873e5343277152d7aa455aad228
ErwanR-Holberton <6218@holbertonstudents.com>
2023-11-25 20:24:37 +0100

    Select all states using alchemy

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Select all states using alchemy"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    s = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3])
+    engine = create_engine(s, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    for state in Session().query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    Session().close()
